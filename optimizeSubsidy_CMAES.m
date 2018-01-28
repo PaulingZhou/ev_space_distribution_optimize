b22d70aebbbdb5c5d4d7b09a9be003db1b647a5f
PaulingZhou <paulingzhou@gmail.com>
2018-01-28 21:32:33 +0800

    完成通过subsidy变量求成本及排队分布的函数
    存储方式使用subsidy_i.mat i为1，2，3

--- optimizeSubsidy_CMAES.m
+++ optimizeSubsidy_CMAES.m
@@ -3,11 +3,11 @@ clear;
 load('neibour_swap_stations.mat');
 opts.LBounds = 0; opts.UBounds = 1.8;
 for i = 1:size(neibour_swap_stations,1)
-    start_time = 860;
+    start_time = 880;
     end_time = 1100;
     for time = start_time:10:end_time
         time
-        neibour_swap_station = neibour_swap_stations{i};
+        neibour_swap_station = neibour_swap_stations{2};
         save('initial_args','time','neibour_swap_station');
         subsidy((time-start_time+10)/10,:) = cmaes('optimize_space_demand', zeros(size(neibour_swap_station,2),1),0.6, opts);
         update_swap_info(subsidy((time-start_time+10)/10,:)');
