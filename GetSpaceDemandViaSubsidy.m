59814da91560f8bcccb25cca67235a1d0f76d8b3
Pauling <paulingzhou@gmail.com>
2018-01-29 19:04:04 +0800

    添加优化前后的空间分布图
    同时更改swap_info.mat至正确状态

--- GetSpaceDemandViaSubsidy.m
+++ GetSpaceDemandViaSubsidy.m
@@ -5,11 +5,13 @@ load('time_swap_dist.mat');
 load('space_ratio.mat');
 load('neibour_swap_stations.mat');
 load('position.mat');
+load('subsidy_1.mat');
 load('subsidy_3.mat');
 load('subsidy_2.mat');
 subsidy = zeros(89,144);
-subsidy(neibour_swap_stations{2},88:110)=subsidy_2';
-subsidy(neibour_swap_stations{3},88:110)=subsidy_3';
+% subsidy(neibour_swap_stations{1},88:110)=subsidy_1';
+% subsidy(neibour_swap_stations{2},88:110)=subsidy_2';
+% subsidy(neibour_swap_stations{3},88:110)=subsidy_3';
 swap_server_continue = zeros(1440,89);    
 swap_server_line = zeros(1440,89,1);   
 line_cost = 0;
@@ -34,5 +36,33 @@ for i = 1:144
     end
 end
 size_time = size(swap_server_line,3);
-hold on;
+figure(1);
 plot(reshape(sum(sum(swap_server_line(:,:,2:size_time))),size_time-1,1),'r');
+hold on;
+swap_line_count = zeros(1440,size(swap_server_line,2)); %swap_line_count(i,k) i时刻k车站的排队车辆数
+for i = 1:size(swap_server_line,1)
+        for k = 1:size(swap_server_line,3)
+            if i+k-1<=1440
+                swap_line_count(i:i+k-1,:) = bsxfun(@plus,swap_line_count(i:i+k-1,:),swap_server_line(i,:,k));
+            end
+        end
+end
+x = position_chargingstation(:,2);
+y =  position_chargingstation(:,1);
+zmax = 30;
+zmin = 0;
+figure();
+for i = 1:8
+    subplot(2,4,i);
+    title('a');
+    [X,Y,Z] = griddata(x,y,swap_line_count(840+30*i,:),linspace(min(x),max(x))',linspace(min(y),max(y)),'v4');
+    pcolor(X,Y,Z);
+    caxis([zmin,zmax]);
+    shading interp
+    hold on
+    n = 6;
+    C = contour(X, Y, Z, n, 'k:');
+end
+colorbar('Location','North','Position',[0.25,0.95,0.515,0.025]);
+set(gcf,'Color','w');
+hold on;
